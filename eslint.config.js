65d6fae232f4f660f5e51f53149fc38aca6b5041
Rajcommit <raj.vbeyond@gmail.com>
2025-07-14 17:57:30 +0530

    added few lines

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from "eslint";
+// eslint.config.js
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-export default defineConfig({
-  files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-  languageOptions: {
-    globals: {
-      ...globals.browser,
+export default [
+  js.configs.recommended,
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
     },
   },
-  plugins: {
-    react: pluginReact,
-  },
-  rules: {
-    ...pluginReact.configs.flat.recommended.rules,
-    ...tseslint.configs.recommended[0].rules, // TS adds as array, so use [0]
-  },
-});
+
+  tseslint.configs.recommended,
+
+  pluginReact.configs.flat.recommended,
+];
